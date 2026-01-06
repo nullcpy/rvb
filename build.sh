1cbb63f44fba4b0a74a1be39ca764d310a886143
geologically <213671755+geologically@users.noreply.github.com>
2026-01-06 19:25:40 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -166,9 +166,11 @@ wait
 rm -rf temp/tmp.*
 if [ -z "$(ls -A1 "${BUILD_DIR}")" ]; then abort "All builds failed."; fi
 
-log "\nInstall [Microg](https://github.com/ReVanced/GmsCore/releases) for non-root YouTube and YT Music APKs"
-log "Use [zygisk-detach](https://github.com/j-hc/zygisk-detach) to detach root ReVanced YouTube and YT Music from Play Store"
-log "\n[revanced-magisk-module](https://github.com/j-hc/revanced-magisk-module)\n"
+log "\n**Notes:**"
+log "• Install [MicroG](https://github.com/WSTxda/MicroG-RE/releases/latest/download/microg-release.apk), required for Google APKs."
+log "• Use [Zygisk Detach](https://github.com/j-hc/zygisk-detach) to stop Play Store from updating Modules."
+log "\n**GitHub:** [ReVanced Builder](https://github.com/geologically/revanced-builder)"
+log "**Telegram:** [ReVanced Builder](https://t.me/revanced_builder)\n"
 log "$(cat "$TEMP_DIR"/*/changelog.md)"
 
 SKIPPED=$(cat "$TEMP_DIR"/skipped 2>/dev/null || :)
