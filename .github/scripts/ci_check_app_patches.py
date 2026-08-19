4507f277ff7ee5e786592edc1b6b1ebf41cae074
Fahim Ahmed <fahim.ahmed1453@gmail.com>
2026-08-19 16:07:27 +0600

    fix: allow multiple apps to match a single patch folder

--- .github/scripts/ci_check_app_patches.py
+++ .github/scripts/ci_check_app_patches.py
@@ -83,17 +83,12 @@ def process_zip(path, pkg_info):
                         an = meta.get('app_name', '')
                         an_clean = an.replace('-', '')
                         
-                        if comp in comp_map: continue
-                        
                         if pf and comp == pf:
-                            comp_map[comp] = pkg
-                            break
+                            comp_map.setdefault(comp, set()).add(pkg)
                         elif an and (comp == an or comp == an_clean):
-                            comp_map[comp] = pkg
-                            break
+                            comp_map.setdefault(comp, set()).add(pkg)
                         elif pkg and comp in pkg.split('.'):
-                            comp_map[comp] = pkg
-                            break
+                            comp_map.setdefault(comp, set()).add(pkg)
             
             if info.filename.endswith('.class'):
                 content = z.read(info)
@@ -102,7 +97,7 @@ def process_zip(path, pkg_info):
                         if m:
                             comp = m.group(1)
                             if comp not in ['shared', 'all']:
-                                comp_map[comp] = pkg
+                                comp_map.setdefault(comp, set()).add(pkg)
 
         comp_regexes = {comp: re.compile(r'(^|/)' + re.escape(comp) + r'(/|\.|-)') for comp in all_comps}
                     
@@ -123,7 +118,8 @@ def process_zip(path, pkg_info):
                 for comp, reg in comp_regexes.items():
                     if reg.search(info.filename):
                         if comp in comp_map:
-                            buckets[comp_map[comp]].update(content)
+                            for p in comp_map[comp]:
+                                buckets[p].update(content)
                         assigned = True # Mark as handled to avoid shared bucket poisoning
                         break
                         
